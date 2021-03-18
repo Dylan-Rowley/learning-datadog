597e256f199b771c299ccf65bcc684458e3e7880
Dylan Rowley <dylan.p.rowley@gmail.com>
2021-03-18 16:28:39 +0000

    Initial Spring Boot project with dependencies added

---
+++ Crypto-Reporter/src/main/java/ie/cryptoreporter/Crypto/Reporter/CryptoReporterApplication.java
@@ -0,0 +1,13 @@
+package ie.cryptoreporter.Crypto.Reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoReporterApplication.class, args);
+	}
+
+}
