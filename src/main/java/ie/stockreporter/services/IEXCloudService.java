44c933fc259da4d84be2c1f676d1e0316ebbf7ea
Dylan Rowley <dylan.p.rowley@gmail.com>
2021-03-20 16:12:01 +0000

    Create a to_do.txt file that will hold ideas/things to complete

--- src/main/java/ie/stockreporter/services/IEXCloudService.java
+++ src/main/java/ie/stockreporter/services/IEXCloudService.java
@@ -1,13 +1,18 @@
 package ie.stockreporter.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.awt.desktop.SystemSleepEvent;
+
 @Service
 public class IEXCloudService {
 
+    @Value("${iex.cloud.api-key}")
+    private String iexCloudApiKey;
 
     @Autowired
     private WebClient webClient;
@@ -18,14 +23,12 @@ public class IEXCloudService {
                 .get()
                 .uri(uriBuilder -> uriBuilder
                 .path("/time-series")
-                .queryParam("token", "FAKE-KEY")
+                .queryParam("token", this.iexCloudApiKey)
                 .build())
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
 
-        System.out.println("The Response from the API is: ");
-        System.out.println(apiResponse);
         return ResponseEntity.ok(apiResponse);
     }
 
