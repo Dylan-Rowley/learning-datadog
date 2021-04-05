2319b331b66643d2b9588bf7fcdc923b84e96663
Dylan Rowley <dylan.p.rowley@gmail.com>
2021-04-05 20:00:19 +0100

    Update localSetup.sh to handle an already present API key

--- localSetup.sh
+++ localSetup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 
-# Recreate the secret
-awslocal secretsmanager create-secret --name iex-cloud-api-key --secret-string Tsk_852b9aad89504dae95cefb487bb4cd9f
+doesSecretExist=$(aws secretsmanager describe-secret --secret-id iex-cloud-api-key)
+
+
+
+# If it does not exist then create the secret using the API key
+if [[ $doesSecretExist == "An error occurred (ResourceNotFoundException)"* ]]; then
+  awslocal secretsmanager create-secret --name iex-cloud-api-key --secret-string Tsk_852b9aad89504dae95cefb487bb4cd9f
+fi
+
+
+if [[ $doesSecretExist == *"\"Name\": \"iex-cloud-api-key\""* ]]; then
+  echo "Secret already exists"
+fi
