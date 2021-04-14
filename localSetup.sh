8801e8e2d61828638918f8d4be9db04e7e4e1e85
Dylan Rowley <dylan.p.rowley@gmail.com>
2021-04-14 20:50:24 +0100

    Update localSetup.sh to run sqs create-queue. Previously sqs was omitted at start of command and command was failing.

--- localSetup.sh
+++ localSetup.sh
@@ -6,7 +6,7 @@ echo "Creating new secret 'iex-cloud-api-key' "
 awslocal secretsmanager create-secret --name iex-cloud-api-key --secret-string Tsk_852b9aad89504dae95cefb487bb4cd9f
 
 echo "Creating Queue"
-awslocal create-queue --queue-name orders
+awslocal sqs create-queue --queue-name orders
 
 echo "END OF SCRIPT"
 
