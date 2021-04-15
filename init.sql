e208d29bc6ac87e069d0913e376d66d33af49486
Dylan Rowley <dylan.p.rowley@gmail.com>
2021-04-15 08:20:34 +0100

    Update init.sql file to create table

--- init.sql
+++ init.sql
@@ -1,6 +1,5 @@
-USE `stock-crypto-reports`
 CREATE TABLE `crypto_trading_pairs` (
-    id long,
-    trading_pair varchar(255),
-    PRIMARY KEY (id)
-)
+    `id` int NOT NULL AUTO_INCREMENT,
+    `trading_pair` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
